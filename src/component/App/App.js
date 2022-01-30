ca20e6d9aef83f2f9469092ed1835156108c8acd
kattana <kattanaluz@gmail.com>
2022-01-30 10:22:56 +0000

    Created components

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-import { Display } from "../../Display";
+import { Display } from "../Display";
 import { Button } from "../Button";
+import { Image } from "../Image";
 import { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { H1 } from "../H1";
 
 function App() {
+  const landText = `Fancy a drink? We got it! Make yourself at home and find out a drink recipe.`;
   const [data, setData] = useState([]);
 
   async function getData() {
@@ -14,14 +18,30 @@ function App() {
     console.log(data.drinks);
     setData(data.drinks[0]);
   }
+
   useEffect(() => {
     getData();
   }, []);
 
   return (
     <div className="App">
-      <Display data={data.strDrink} />
-      <Button />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            ((<Image src={"../../landimage.png"} alt={"in drink we trust"} />),
+            (<H1 text={landText} />))
+          }
+        />
+        <Route
+          path="randomdrinks"
+          element={
+            ((<Image src={"../../landimage.png"} alt={"in drink we trust"} />),
+            (<Display data={data.strDrink} />),
+            (<Button text={"get a cocktail"} />))
+          }
+        />
+      </Routes>
     </div>
   );
 }
