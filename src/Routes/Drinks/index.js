6ff6375a159307d401d7280da7e68ac27d60e835
kattana <kattanaluz@gmail.com>
2022-02-02 19:17:05 +0000

    Added styles to drink page

--- src/Routes/Drinks/index.js
+++ src/Routes/Drinks/index.js
@@ -1,11 +1,14 @@
 import Paragraph from "../../Component/Paragraph";
 import H1 from "../../Component/H1";
+import H2 from "../../Component/H2";
 import { useState, useEffect } from "react";
 import Input from "../../Component/Input";
 import css from "./index.module.css";
 
 export function Drinks() {
-  const placeHolder = `type here and find a drink recipe`;
+  const inputText = `type here and find a drink recipe`;
+  const ingredientsTitle = `Ingredients list`;
+  const measuresTitle = `Measures`;
 
   const [input, setInput] = useState();
   function getInputText(e) {
@@ -26,43 +29,53 @@ export function Drinks() {
         `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${text}`
       );
       const data = await response.json();
-      console.log(`this is your data from api: ${data.drinks[0].strDrink}`);
-      setData(data.drinks[0].strDrink);
-      setData(...data, data.drinks[0].strInstructions);
-      console.log(data);
-      getIngredients();
+      setData(data.drinks[0]);
+      console.log(data.drinks[0]);
     }
     getData();
   }, [text]);
 
-  function getIngredients() {
-    const ingredients = [];
-    for (let i = 0; i < 16; i++) {
-      if (data.strIngredient + i) {
-        ingredients.push(data.strIngredient + i);
-        console.log(ingredients);
-      }
-    }
-  }
+  if (data)
+    return (
+      <div>
+        <Input
+          placeholder={inputText}
+          divClassName={css.divInput}
+          inputClassName={css.searchInput}
+          onChange={getInputText}
+          onKeyUp={handleEnter}
+        />
+        <div className={css.recipeContainer}>
+          <H1 text={data.strDrink} h1ClassName={css} />
+          <div className={css.recipeIngAndMeas}>
+            <div className={css.recipeMeasure}>
+              <H2 text={measuresTitle} />
+              <Paragraph text={data.strMeasure1} />
+              <Paragraph text={data.strMeasure2} />
+              <Paragraph text={data.strMeasure3} />
+              <Paragraph text={data.strMeasure4} />
+              <Paragraph text={data.strMeasure5} />
+              <Paragraph text={data.strMeasure6} />
+              <Paragraph text={data.strMeasure7} />
+              <Paragraph text={data.strMeasure8} />
+            </div>
 
-  return (
-    <div>
-      <Input
-        placeholder={placeHolder}
-        divClassName={css.divInput}
-        inputClassName={css.searchInput}
-        onChange={getInputText}
-        onKeyUp={handleEnter}
-      />
-      <div className={css.recipe}>
-        <H1 text={data} h1ClassName={css} />
-        <Paragraph text={data.strIngredient1} />
-        <Paragraph text={data.strIngredient2} />
-        <Paragraph text={data.strIngredient3} />
-        <Paragraph text={data.strIngredient4} />
-        <Paragraph text={data.strIngredient5} />
-        <Paragraph text={data.strInstructions} />
+            <div className={css.recipeIngredients}>
+              <H2 text={ingredientsTitle} />
+              <Paragraph text={data.strIngredient1} />
+              <Paragraph text={data.strIngredient2} />
+              <Paragraph text={data.strIngredient3} />
+              <Paragraph text={data.strIngredient4} />
+              <Paragraph text={data.strIngredient5} />
+              <Paragraph text={data.strIngredient6} />
+              <Paragraph text={data.strIngredient7} />
+              <Paragraph text={data.strIngredient8} />
+            </div>
+          </div>
+          <div className={css.recipeInstructions}>
+            <Paragraph text={data.strInstructions} />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
